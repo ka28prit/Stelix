2da5ac5e9a74a69014c42641b5d4f71bb4758db2
Keith P <95986248+ka28prit@users.noreply.github.com>
2026-03-24 20:40:46 -0400

    Implement fits_to_csv function
    Add function to convert .fits files to .csv format

---
+++ stelix-reduce/pipeline.py
@@ -0,0 +1,18 @@
+from astropy.io import fits
+import pandas as pd
+
+def fits_to_csv(input_file, output_file=None):
+  """
+  Converts .fits file to .csv file
+  """
+
+  with fits.open(input_file) as hdil:
+    data = hdul[1].data
+
+  df = pd.DataFrame(data)
+
+  if not output_file:
+    output_file = input_file.replace(".fits",".csv")
+
+  df.to_csv(output_file, index=False)
+  print(f"Converted {input_file} -> {output_file}")
